9e3988c1d4f80cc92a69056352b422fd26e68f48
Furydrack <marius.boulandet@gmail.com>
2025-10-28 10:50:05 +0100

    feat: Draggable Object and Layers manager

---
+++ Assets/_Game/Scripts/Managers/ObjectsManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections.Generic;
+using TriInspector;
+
+public class ObjectsManager : MonoBehaviour
+{
+    public static ObjectsManager instance;
+
+    [Title("Runtime")]
+    [SerializeField, ReadOnly]
+    List<GameObject> _interractableObjects = new List<GameObject>();
+    [ReadOnly]
+    public int highestSortingOrder;
+
+    private void Awake()
+    {
+        if(instance == null)
+            instance = this;
+    }
+
+    private void Start()
+    {
+        _interractableObjects.AddRange(GameObject.FindGameObjectsWithTag("DraggableObject"));
+
+        foreach (GameObject obj in _interractableObjects)
+            if (obj.GetComponent<SpriteRenderer>().sortingOrder > highestSortingOrder)
+                highestSortingOrder = obj.GetComponent<SpriteRenderer>().sortingOrder;
+    }
+}
